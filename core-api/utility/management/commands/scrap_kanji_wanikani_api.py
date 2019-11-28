c2a6a5ae19e32d9462248c07858d4f61fc4cfc38
andika <dika.kusuma23@gmail.com>
2019-11-28 22:11:01 +0700

    Update scrapper

--- core-api/utility/management/commands/scrap_kanji_wanikani_api.py
+++ core-api/utility/management/commands/scrap_kanji_wanikani_api.py
@@ -7,22 +7,24 @@ from apps.lesson.models import KanjiReadings
 
 
 K_WANIKANI_API_ENDPOINT = 'https://www.wanikani.com/api/user/0366768dc874416f4257f4e0486de9a4'
-K_WANIKANI_MAX_LEVEL = 1
 
 class Command(BaseCommand):
     help = 'Scrap kanji from wanikani APIs'
 
+    def add_arguments(self, parser):
+        parser.add_argument('levels', nargs='+', type=int)
+
     def handle(self, *args, **options):
-        for level in range(1, K_WANIKANI_MAX_LEVEL + 1):
+        for level in options['levels']:
             url = '{}/kanji/{}'.format(K_WANIKANI_API_ENDPOINT, level)
             r = requests.get(url)
             result = r.json()
-            for kanji in result['requested_information']:
-                writing = kanji['character']
-                meaning = kanji['meaning']
-                onyomi = kanji['onyomi']
-                kunyomi = kanji['kunyomi']
-                important_reading = kanji['important_reading']
+            for kanji_data in result['requested_information']:
+                writing = kanji_data['character']
+                meaning = kanji_data['meaning']
+                onyomi = kanji_data['onyomi']
+                kunyomi = kanji_data['kunyomi']
+                important_reading = kanji_data['important_reading']
                 kanji, _ = Kanji.objects.get_or_create(writing=writing, meaning=meaning, level=level, important_reading=important_reading)
                 if onyomi:
                     onyomi_reading = KanjiReadings.objects.get_or_create(kanji=kanji, reading_type=constants.ONYOMI, reading=onyomi)
